2dc12ab5b3f570396c44f10cc39feca8637cee0b
ryo-ma <saka_ro@yahoo.co.jp>
2021-09-10 13:59:37 +0900

    Update part3

--- FIWAREBasic/fiware-part3/setup.sh
+++ FIWAREBasic/fiware-part3/setup.sh
@@ -1,6 +1,7 @@
 pip2 install Flask==1.0.2
 pip2 install pyOpenSSL==19.0.0
 wget https://raw.githubusercontent.com/telefonicaid/fiware-orion/master/scripts/accumulator-server.py
+chmod a+x accumulator-server.py
 wget https://github.com/telefonicaid/fiware-orion/raw/master/docker/docker-compose.yml
 docker-compose up -d
 rm docker-compose.yml
